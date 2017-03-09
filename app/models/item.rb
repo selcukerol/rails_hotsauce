4eb9a6f44c28f6344b20da6ebb4e5623b65be3d0
Selcuk Erol <selcuk_erol@hotmail.com>
2017-03-09 11:41:39 -0500

    Model revision

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
 has_attached_file :image, styles: {medium: "300x300>", thumb: "150x150"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/	
 
+	has_many :order_items
 	has_many :orders, :through => :order_items
 
 	def label
