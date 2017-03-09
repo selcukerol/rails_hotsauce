c884777e3106dc2b28674d1f691c01cd5644328d
Selcuk Erol <selcuk_erol@hotmail.com>
2017-03-09 12:24:35 -0500

    Model revision2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Item.create( [{name:'Turkish Pepper', cost: 40}, {name:'Sweet Pepper', cost: 10}, {name:'Sumac', cost: 100}, {name:'Serrano', cost: 30}, {name:'New Mexico Chilli', cost: 20}, {name:'Cayenne', cost: 40} ]) 
+
+def set_image_path image
+	File.new( File.join Rails.root, %w(public assets images),image)
+end	
+
+
+
+Item.create( [{name:'Turkish Pepper', cost: 40, image: set_image_path('turkishpepper.jpg')}, {name:'Sweet Pepper', cost: 10, image: set_image_path('sweetpepper.jpg')}, {name:'Sumac', cost: 100, image: set_image_path('sumac.jpeg')}, {name:'Serrano', cost: 30, image: set_image_path('serrano.jpg')}, {name:'New Mexico Chilli', cost: 20, image: set_image_path('mexican.jpg')}, {name:'Cayenne', cost: 40, image: set_image_path('cayenne.jpg')} ]) 
